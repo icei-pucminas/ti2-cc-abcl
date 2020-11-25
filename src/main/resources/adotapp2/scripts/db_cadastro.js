bc735281d2e510b48b32c68ebf0a4e14f36be140
MIOJOLU <parentelulu@gmail.com>
2020-11-25 16:45:01 -0300

    Atualizações Página de Perfil -> Mudanças

--- src/main/resources/adotapp2/scripts/db_cadastro.js
+++ src/main/resources/adotapp2/scripts/db_cadastro.js
@@ -15,24 +15,7 @@ var questionario = [
         "pergunta": "Você já participa ou completou as aulas de capacitação parental?"
     }
 ];
-function togglePopup(){
-    document.getElementById("popup-1").classList.toggle("active");
-  }
-function togglePopup2(){
-    document.getElementById("popup-2").classList.toggle("active");
-}
-function togglePopup3(){
-    document.getElementById("popup-3").classList.toggle("active");
-}
-function togglePopup4(){
-    document.getElementById("popup-4").classList.toggle("active");
-}
-function togglePopup5(){
-    document.getElementById("popup-5").classList.toggle("active");
-}
-function togglePopup6(){
-    document.getElementById("popup-6").classList.toggle("active");
-}
+  
 
 onload = () => {
     // mostraUsuario();
