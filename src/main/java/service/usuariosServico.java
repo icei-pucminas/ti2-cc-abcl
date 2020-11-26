7b720860bdcb218540368bf22dd721bf5f1e4a95
MIOJOLU <parentelulu@gmail.com>
2020-11-25 23:04:28 -0300

    Atualizações na página de login

--- src/main/java/service/usuariosServico.java
+++ src/main/java/service/usuariosServico.java
@@ -9,47 +9,46 @@ import model.Usuario;
 
 public class usuariosServico {
 	private DAOUsuarios dao;
-	
+
 	public usuariosServico() {
 		dao = new DAOUsuarios();
 	}
-		
-	public Object addUsuarios (Request request, Response response){
+
+	public Object addUsuarios(Request request, Response response) {
 		response.header("Content-Type", "application/json");
 		dao.conectar();
-		
+
 		String nome = request.queryParams("nome");
 		String sobrenome = request.queryParams("sobrenome");
 		String email = request.queryParams("email");
 		String telefone = request.queryParams("telefone");
 		String senha = request.queryParams("senha");
-		
+
 		Usuario user = new Usuario(nome, sobrenome, senha, email, telefone, 1);
-		
+
 		System.out.println(user.toString());
-		
-		if(DAOUsuarios.getUsuario(email) == null){
+
+		if (DAOUsuarios.getUsuario(email) == null) {
 			dao.addUsuarios(user);
 			response.status(201);
-		}else{
-			//NÃO MANDAR RESPOSTA DE ERRO!!!
+		} else {
+			// NÃO MANDAR RESPOSTA DE ERRO!!!
 			response.status(400);
 		}
 
 		dao.close();
-		
+
 		return user.toJson();
-		
 	}
-	
+
 	public Object updateUsuario(Request request, Response response) {
 		System.out.println("cheguei aqui - user");
-        dao.conectar();
-            
+		dao.conectar();
+
 		int id = Integer.parseInt(request.queryParams("id"));
-		
-		Usuario user = dao.getUsuario(id);  
-	    
+
+		Usuario user = DAOUsuarios.getUsuario(id);
+
 		dao.atualizarStatusProcesso(user);
 
 		response.status(201); // 201 Created
@@ -57,15 +56,14 @@ public class usuariosServico {
 		return user.toJson();
 	}
 
-	
-	public Object getUsuario (Request request, Response response){
+	public Object getUsuario(Request request, Response response) {
 		response.header("Content-Type", "application/json");
 		dao.conectar();
-		
+
 		String email = request.queryParams("email");
 
-		Usuario user = dao.getUsuario(email);
-		
+		Usuario user = DAOUsuarios.getUsuario(email);
+
 		DAOProcessos daoP = new DAOProcessos();
 		daoP.conectar();
 		Processo p = new Processo(false, false, user.getId(), 1, "inicio");
@@ -74,44 +72,43 @@ public class usuariosServico {
 
 		daoP.close();
 		dao.close();
-		
+
 		return user.toJson();
 	}
-	
-	public Object loginUsuario (Request request, Response response){
+
+	public Object loginUsuario(Request request, Response response) {
 		response.header("Content-Type", "application/json");
 		dao.conectar();
-		
+
 		String email = request.queryParams("email");
 		String senha = request.queryParams("senha");
 
-		Usuario user = dao.loginUsuario(email, senha);
+		Usuario user = DAOUsuarios.loginUsuario(email, senha);
 		response.status(201);
 
 		dao.close();
-		
+
 		return user.toJson();
-		
 	}
-	
+
 	// public Object Usuarios(Request request, Response response) {
-	// 	dao.conectar();
+	// dao.conectar();
 
-	// 	StringBuffer valores = new StringBuffer("[");
-	// 	int cont = 0;
-	// 	Gson gson = new GsonBuilder().create();
-	// 	for(Usuario user : DAOUsuarios.getUsuarios()){
-	// 		valores.append(gson.toJSON(user));
-	// 		cont++;
-	// 		if(cont != DAOUsuarios.getUsuarios().length)
-	// 			valores.append(",\n");
-	// 	}
-	// 	valores.append("]");
+	// StringBuffer valores = new StringBuffer("[");
+	// int cont = 0;
+	// Gson gson = new GsonBuilder().create();
+	// for(Usuario user : DAOUsuarios.getUsuarios()){
+	// valores.append(gson.toJSON(user));
+	// cont++;
+	// if(cont != DAOUsuarios.getUsuarios().length)
+	// valores.append(",\n");
+	// }
+	// valores.append("]");
 
-	// 	response.header("Content-Type", "application/json");
-    //     response.header("Content-Encoding", "UTF-8");
-	// 	dao.close();
+	// response.header("Content-Type", "application/json");
+	// response.header("Content-Encoding", "UTF-8");
+	// dao.close();
 
-	// 	return valores.toString();
+	// return valores.toString();
 	// }
 }
--- src/main/resources/adotapp2/scripts/bancodedados/usuarios_Cadastrados.js
+++ src/main/resources/adotapp2/scripts/bancodedados/usuarios_Cadastrados.js
@@ -9,11 +9,21 @@ function usuarioCadastrado() {
 
 	let req2 = new XMLHttpRequest;
 	    req2.open("get", "/loginUsuario?email=" + email + "&senha=" + senha, true);
-		req2.onreadystatechange = () => {
+		req2.onload = () => {
 			if(req2.readyState == 4 && req2.status == 201){
-				let string = req2.responseText;
+				let dados = JSON.parse(req2.responseText);
 				window.location.href = "../HTML/home.html";
-				localStorage.setItem('usuario_ativo', string);
+				let filta = {
+					"nome": dados.nome,
+					"sobrenome": dados.sobrenome,
+					"id": dados.id,
+					"email": dados.email,
+					"processo_ativo": dados.processo_ativo
+				}
+				localStorage.setItem('usuario_ativo', JSON.stringify(filta));
+			}else{
+				$('#senhaIncorreta').fadeIn('fast')
+				$('#senhaIncorreta'),delay(200).fadeTo("slow");
 			}
 		};	
 		req2.send();
